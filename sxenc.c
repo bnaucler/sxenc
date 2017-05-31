fb0df4ea4b1230d43938c3fb2d695975c378650c
bnaucler <mail@bnaucler.se>
2017-05-31 13:33:29 +0200

    Adding keygen()

--- sxenc.c
+++ sxenc.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define ENCKEY "this is the key"
 #define FEXT ".ap"
+#define KEYFILE ".sxkey"
 
+#define CHARSET "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+#define KEYLEN 512
 #define FNLEN 128
 #define MBCH 1024
 
 #define M_ENCRYPT 0
 #define M_DECRYPT 1
+#define M_GENKEY 2
 
 typedef struct flag {
 	int mfl;
@@ -20,7 +25,7 @@ int die(char *err, int ret) {
 
 	if(err[0]) fprintf(stderr, "Error: %s\n", err);
 
-	return ret;
+	exit(ret);
 }
 
 char *crypt(char *dst, const char *src, const char *key, size_t sz) {
@@ -36,6 +41,19 @@ char *crypt(char *dst, const char *src, const char *key, size_t sz) {
 	*dst = 0;
 
 	return dst;
+}
+
+char *getkey(char *key, int klen) {
+
+	char fname[FNLEN];
+	snprintf(fname, FNLEN, "%s%c%s", getenv("HOME"), '/', KEYFILE);
+
+	FILE *fp = fopen(fname, "r");
+	if(!fp) die("Could not open key file (run with -g (size) to generate)", 4);
+
+	fread(key, 1, klen, fp);
+
+	return key;
 }
 
 int encloop(FILE *src, FILE *dst, char *key, flag *f) {
@@ -60,12 +78,15 @@ int execop(flag *f, char **argv) {
 
 	FILE *src, *dst;
 
+	char *key = calloc(KEYLEN + 1, sizeof(char));
+	getkey(key, KEYLEN);
+
 	do {
 		src = fopen(*argv, "r");
 		if(!src) return die("Cannot open file", 2);
 
 		if(f->mfl == M_DECRYPT) {
-			encloop(src, NULL, ENCKEY, f);
+			encloop(src, NULL, key, f);
 
 		} else {
 			strncat(*argv, FEXT, FNLEN); // hack
@@ -73,7 +94,7 @@ int execop(flag *f, char **argv) {
 			dst = fopen(*argv, "w");
 			if(!dst) return die("Cannot create encrypted file", 3);
 
-			encloop(src, dst, ENCKEY, f);
+			encloop(src, dst, key, f);
 
 			fclose(dst);
 		}
@@ -81,6 +102,33 @@ int execop(flag *f, char **argv) {
 		fclose(src);
 
 	} while(*++argv);
+
+	free(key);
+	return 0;
+}
+
+int keygen(flag *f, char **argv) {
+
+	time_t t;
+	srand((unsigned) time(&t));
+
+	unsigned int a = 0;
+
+    int klen = (int) strtol(*argv, NULL, 10);
+	if(klen > KEYLEN || klen < 10) klen = KEYLEN;
+
+	char key[(KEYLEN + 1)];
+	int cslen = strlen(CHARSET);
+
+	for(a = 0; a < klen; a++) key[a] = CHARSET[(rand() % cslen)];
+
+	char fname[FNLEN];
+	snprintf(fname, FNLEN, "%s%c%s", getenv("HOME"), '/', KEYFILE);
+
+	FILE *fp = fopen(fname, "w");
+	if(!fp) die("Could not write to key file", 3);
+
+	fwrite(key, 1, strlen(key) + 1, fp);
 
 	return 0;
 }
@@ -94,6 +142,10 @@ void setfl(char *arg, flag *f) {
 				f->mfl = M_DECRYPT;
 				break;
 
+			case 'g':
+				f->mfl = M_GENKEY;
+				break;
+
 			case 'v':
 				f->vfl++;
 				break;
@@ -103,11 +155,17 @@ void setfl(char *arg, flag *f) {
 
 int main(int argc, char **argv) {
 
+	int ret = 0;
+
 	flag *f = calloc(1, sizeof(flag));
 
 	if(argc < 2 || (argc == 2 && argv[1][0] == '-'))
-		return die("No file specified", 1);
+		die("Not enough arguments", 1);
 	else if (argv[1][0] == '-') setfl(argv++[1], f);
 
-	return execop(f, ++argv);
+	if(f->mfl == M_GENKEY) ret =  keygen(f, ++argv);
+	else ret = execop(f, ++argv);
+
+	free(f);
+	return ret;
 }
